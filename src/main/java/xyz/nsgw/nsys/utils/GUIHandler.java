fe19ebe7e2883b5c2e716712bf6bf549e6a422cb
nsgwick <nickgoespop@gmail.com>
2021-09-09 22:07:44 +0100

    Started GUIs

--- src/main/java/xyz/nsgw/nsys/utils/GUIHandler.java
+++ src/main/java/xyz/nsgw/nsys/utils/GUIHandler.java
@@ -4,11 +4,11 @@ import dev.triumphteam.gui.builder.item.ItemBuilder;
 import dev.triumphteam.gui.guis.Gui;
 import dev.triumphteam.gui.guis.GuiItem;
 import dev.triumphteam.gui.guis.PaginatedGui;
+import dev.triumphteam.gui.guis.ScrollingGui;
 import net.kyori.adventure.text.Component;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import xyz.nsgw.nsys.storage.objects.Profile;
 
 public class GUIHandler {
@@ -16,6 +16,26 @@ public class GUIHandler {
     private PaginatedGui menu;
 
     public GUIHandler() {
+    }
+
+    public void profile(Profile profile, Player displayTo) {
+        ScrollingGui gui = Gui.scrolling()
+                .rows(6)
+                .pageSize(45)
+                .create();
+        ItemStack skull = ItemBuilder.skull().owner(Bukkit.getOfflinePlayer(profile.getKey()))
+                .lore().build();
+    }
+
+    public void profile(Profile profile) {
+        ScrollingGui gui = Gui.scrolling()
+                .rows(6)
+                .pageSize(45)
+                .create();
+        ItemStack skull = ItemBuilder.skull().owner(Bukkit.getOfflinePlayer(profile.getKey()))
+                .lore().build();
+        gui.getFiller().fill(ItemBuilder.from(Material.BLACK_STAINED_GLASS_PANE).name(Component.text(ChatColor.DARK_GRAY+"-")).asGuiItem());
+        gui.setItem(0,0,ItemBuilder.from(skull).name(Component.text(ChatColor.WHITE+Bukkit.getOfflinePlayer(profile.getKey()).getName())).asGuiItem());
     }
 
     public void homes(Profile profile, Player player) {
